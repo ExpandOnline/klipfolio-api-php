a782733730f5ba5689788a652211644fb1471780
Ron Oudgenoeg <roudgenoeg@expandonline.nl>
2016-10-03 14:45:31 +0200

    s85.9 Add tab share

---
+++ src/Object/Tab/TabShare.php
@@ -0,0 +1,65 @@
+<?php namespace ExpandOnline\KlipfolioApi\Object\Tab;
+
+use ExpandOnline\KlipfolioApi\Object\BaseApiResource;
+
+/**
+ * Class Tab
+ * @package ExpandOnline\KlipfolioApi\Object\Tab
+ *
+ * @property bool $can_edit
+ * @property string $tabId
+ * @property array $groups
+ */
+class TabShare extends BaseApiResource
+{
+    /**
+     * @return boolean
+     */
+    public function isCanEdit() {
+        return $this->can_edit;
+    }
+
+    /**
+     * @param boolean $can_edit
+     * @return $this
+     */
+    public function setCanEdit($can_edit) {
+        $this->can_edit = $can_edit;
+        return $this;
+    }
+
+    /**
+     * @param $groupId
+     * @param $canEdit
+     */
+    public function addShare($groupId, $canEdit) {
+        $this->data['groups'][] = [
+            'group_id' => $groupId,
+            'can_edit' => $canEdit
+        ];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getShares() {
+        return $this->data['groups'];
+    }
+
+    /**
+     * @return string
+     */
+    public function getTabId() {
+        return $this->tabId;
+    }
+
+    /**
+     * @param string $tabId
+     */
+    public function setTabId($tabId) {
+        $this->tabId = $tabId;
+    }
+
+
+
+}
