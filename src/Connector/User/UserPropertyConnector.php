2502873cc56dea15766c3c2e65de9bf2d8213cec
Peter Vaassens <pvaassens@expandonline.nl>
2016-11-24 12:31:56 +0100

    S89.5 Adds UserProperties object and connectors

---
+++ src/Connector/User/UserPropertyConnector.php
@@ -0,0 +1,60 @@
+<?php namespace ExpandOnline\KlipfolioApi\Connector\User;
+
+use ExpandOnline\KlipfolioApi\Connector\BaseApiResourceConnector;
+use ExpandOnline\KlipfolioApi\Object\User\UserProperties;
+
+/**
+ * Class UserPropertyConnector
+ * @package ExpandOnline\KlipfolioApi\Connector\Client
+ *
+ */
+class UserPropertyConnector extends BaseApiResourceConnector
+{
+    /**
+     * @var UserProperties
+     */
+    protected $resource;
+    /**
+     * @param string $option
+     * @return string
+     */
+    public function getEndpoint($option = '')
+    {
+        if (empty($this->getId())) {
+            throw new \InvalidArgumentException('Missing required user id in user property resource.');
+        }
+
+        return 'users/' . $this->getId() . '/properties';
+
+    }
+
+    /**
+     * @return bool
+     */
+    public function canRead()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function canUpdate() {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    public function canCreate() {
+        return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getObjectName()
+    {
+        return UserProperties::class;
+    }
+}
---
+++ src/Object/User/UserProperties.php
@@ -0,0 +1,39 @@
+<?php namespace ExpandOnline\KlipfolioApi\Object\User;
+
+use ExpandOnline\KlipfolioApi\Object\BaseApiResource;
+
+/**
+ * Class UserProperties
+ * @package ExpandOnline\KlipfolioApi\Object\ClientProprty
+ *
+ * @property array properties
+ */
+class UserProperties extends BaseApiResource {
+
+
+    /**
+     * @param array $properties
+     * @return $this
+     */
+    public function setProperties(array $properties) {
+        $this->properties = $properties;
+        return $this;
+    }
+
+    /**
+     * @param string $name
+     * @param string $value
+     * @return $this
+     */
+    public function addProperty($name, $value) {
+        $this->properties[$name] = $value;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getProperties() {
+        return $this->properties;
+    }
+}
